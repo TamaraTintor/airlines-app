3ca9be90b9bd97775331970e096070737abe9677
Jelena Šućur <jelenas93@yahoo.com>
2019-11-08 18:16:38 +0100

    dodajem supervizorhome izmjena

--- src/pages/SupervizorHome.jsx
+++ src/pages/SupervizorHome.jsx
@@ -2,39 +2,56 @@ import React, { Component } from 'react';
 import { Button, Modal, ModalBody, InputGroup, InputGroupAddon, Container, Table, Input } from 'reactstrap';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
+import '../index.css';
 
 class SupervizorHome extends Component {
 
     constructor(props) {
         super(props);
 
-    /*    checkIfLogged().then(resp => {
-            if (!resp) {
-                this.props.history.push('/')
-            }
-        });*/
+        /*    checkIfLogged().then(resp => {
+                if (!resp) {
+                    this.props.history.push('/')
+                }
+            });*/
 
-      //  this.logOut = this.logOut.bind(this);
+        //  this.logOut = this.logOut.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
-
-
-        this.state = { users: [], showModal: false, message: "", username: "", mail: "", password:"" }
+        this.loadDataAktivni = this.loadDataAktivni.bind(this);
+        this.handleCheckBox = this.handleCheckBox.bind(this);
+        //this.pretvaranjeUString = this.pretvaranjeUString.bind(this);
+        this.state = { users: [], showModal: false, message: "", username: "", mail: "", password: "", isActive: "" }
 
     }
 
+    loadDataAktivni() {
+        fetch('/api/user/aktivni')
+            .then(response => response.json())
+            .then(data => this.setState({ users: data }));
+    }
+ 
     loadData() {
         fetch('/api/user')
             .then(response => response.json())
             .then(data => this.setState({ users: data }));
     }
 
-    componentWillMount() {
+    componentWillMount() { 
         this.loadData();
     }
 
+    handleCheckBox(event) {
+        var checkbox = document.getElementById("checkbox_aktivni");
+        if (checkbox.checked == true) {
+            this.loadDataAktivni();
+        } else {
+            this.loadData();
+        }
+    }
+
     cleanData() {
-        this.setState({ message: "", username: "", mail: "", password:""});
+        this.setState({ message: "", username: "", mail: "", password: "", isActive:"" });
     }
 
     toggle = field => {
@@ -43,18 +60,18 @@ class SupervizorHome extends Component {
         });
     };
 
-  /*  logOut() {
-        fetch('/auth/logout',
-            {
-                method: 'GET',
-                mode: 'cors',
-                headers:
-                {
-                    credentials: 'include'
-                },
-            }
-        ).catch(() => this.props.history.push('/'));
-    }*/
+    /*  logOut() {
+          fetch('/auth/logout',
+              {
+                  method: 'GET',
+                  mode: 'cors',
+                  headers:
+                  {
+                      credentials: 'include'
+                  },
+              }
+          ).catch(() => this.props.history.push('/'));
+      }*/
 
 
 
@@ -68,7 +85,7 @@ class SupervizorHome extends Component {
             username: this.state.username,
             password: this.state.password,
             mail: this.state.mail
-     }
+        }
 
         fetch('/api/user',
             {
@@ -78,14 +95,19 @@ class SupervizorHome extends Component {
 
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
-                   // credentials: 'include'
+                    // credentials: 'include'
                 },
-                mode:'cors',
-                credentials:'include',
+                mode: 'cors',
+                credentials: 'include',
                 body: JSON.stringify(dataToSend),
             }
-        ).then(response => { if (response.status === 200) { this.loadData(); this.cleanData(); this.toggle('showModal'); toast.success("Korisnik sacuvan", { position: toast.POSITION_TOP_RIGHT }); } else { this.setState({ message: response.status }) } });
+        ).then(response => { if (response.status === 202) { this.loadData(); this.cleanData(); this.toggle('showModal'); toast.success("Korisnik sacuvan", { position: toast.POSITION_TOP_RIGHT }); } else { this.setState({ message: "Grska prilikom dodavanja korisnika" }) } });
     }
+
+  /*  pretvaranjeUString(boolean) {
+        //var bool=true;
+        return boolean.toString();
+    }*/
 
     render() {
         console.log("RENDER:")
@@ -93,14 +115,12 @@ class SupervizorHome extends Component {
         let users = [...this.state.users];
         return (
             <div style={{ backgroundColor: '#923cb5', backgroundImage: `linear-gradient(150deg, #000000 30%, #923cb5 70%)`, margin: 0, height: '100vh', width: '100%', justifyContent: 'center', alignItems: 'center', }}>
-                <ToastContainer autoClose={5000} />
+                <ToastContainer autoClose={3000} />
                 <Container>
                     <Modal
                         isOpen={this.state.showModal}
                         toggle={() => this.toggle('showModal')}
-                        className="bg-transparent modal-xl"
-
-                    >
+                        className="bg-transparent modal-xl">
                         <ModalBody>
                             <div>
                                 <InputGroup size="sm">
@@ -108,7 +128,7 @@ class SupervizorHome extends Component {
                                         Username:
                                     </InputGroupAddon>
                                     <Input
-                                        type="text" name="username" id="username" value={this.state.username} onChange={this.handleInputChange}
+                                        type="text" name="username" id="username" value={this.state.username} onChange={this.handleInputChange} placeholder="username"
                                     ></Input>
                                 </InputGroup>
                                 <InputGroup size="sm">
@@ -118,7 +138,9 @@ class SupervizorHome extends Component {
                                     <Input
                                         type="text" name="mail" id="mail" value={this.state.mail} onChange={(event) => this.handleInputChange(event)}
                                     ></Input>
-                                     <InputGroupAddon addonType="prepend">
+                                </InputGroup>
+                                <InputGroup size="sm">
+                                    <InputGroupAddon addonType="prepend">
                                         Password:
                                      </InputGroupAddon>
                                     <Input
@@ -136,21 +158,35 @@ class SupervizorHome extends Component {
                     <Table>
                         <tbody>
                             <tr>
-                                <td><h1 style={{ color: "#923cb5" }}>Users Page</h1></td>
+                                <td><h1 style={{ color: "#923cb5" }}>Supervizor Page</h1></td>
                             </tr>
                         </tbody>
                     </Table>
                 </Container>
                 <Container>
-                    <Button style={{ backgroundColor: "#923cb5" }} onClick={() => this.toggle('showModal')}>Dodaj novog korisnika</Button>
+                    <Table borderless="0">
+                        <tr><td><Button style={{ backgroundColor: "#923cb5" }} onClick={() => this.toggle('showModal')}>Dodaj novog korisnika</Button></td>
+                            <td align="right"> <p><font color="white">Prikazi aktivne korisnike:</font></p> </td>
+                            <td align="right"><input type="checkbox" id="checkbox_aktivni" onChange={(event) => this.handleCheckBox(event)}></input></td>
+                        </tr>
+                    </Table>
+
                     <Table >
                         <thead>
-                            <tr><th>ID</th><th>Username</th><th>Email</th><th>Password</th><th>IsActive</th></tr>
+                            <tr><th>ID</th><th>Username</th><th>Email</th><th>Password</th><th>Aktivan</th></tr>
                         </thead>
                         <tbody>
                             {
                                 users.map((user) => {
-                                    return <tr key={user.id}><td>{user.id}</td><td>{user.username}</td><td>{user.mail}</td><td>{user.password}</td><td>{JSON.stringify(user.isActive)}</td></tr>
+                                    return <tr key={user.id}>
+                                        <td>{user.id}</td>
+                                        <td>{user.username}</td>
+                                        <td>{user.mail}</td>
+                                        <td>{user.password}</td>
+                                        <td>{user.active}</td>
+                                        </tr>
+                                   
+                                
                                 })
                             }
                         </tbody>
